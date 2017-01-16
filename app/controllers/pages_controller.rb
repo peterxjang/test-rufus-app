b10ab6c7cea6c39b6d3ae7c368d114082aa3bc2f
Peter Jang <peter.x.jang@gmail.com>
2017-01-16 11:37:21 -0600

    Stop and restart rufus from controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,4 +2,14 @@ class PagesController < ApplicationController
   def home
     render 'home.html.erb'
   end
+
+  def stop
+    puts 'Stopping rufus'
+    Rufus::Scheduler.singleton.jobs(:tag => 'main_process').each do |job|
+      Rufus::Scheduler.singleton.unschedule(job)
+    end
+    puts 'Starting rufus'
+    load "#{Rails.root}/config/initializers/task_scheduler.rb"
+    redirect_to "/"
+  end
 end
--- config/initializers/task_scheduler.rb
+++ config/initializers/task_scheduler.rb
@@ -1,6 +1,6 @@
 unless defined?(Rails::Console) || File.split($0).last == 'rake'
   s = Rufus::Scheduler.singleton
-  s.every '3s' do
+  s.every '3s', :tag => 'main_process' do
     Rails.logger.info "hello, it's #{Time.now}"
     Rails.logger.flush
     Task.create(name: "yo", priority: (1..10).to_a.sample)
